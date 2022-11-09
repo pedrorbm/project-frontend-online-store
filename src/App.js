86d9c0efb5a2b054423c405b251ca1db2da3a606
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2022-11-09 18:00:46 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import ShoppingCart from './Pages/ShoppingCart';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Switch>
+        <Route path="/shoppingcart" component={ ShoppingCart } />
         <Route exact path="/" component={ Home } />
       </Switch>
     );

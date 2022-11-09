a3a5486b956d5e57bcdf8477ba0551ce0e1253c0
Pedro Roberto Bezerra De Medeiros <pedropwks@gmail.com>
2022-11-09 18:10:35 -0300

    Adiciona requisito 4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import Category from './components/Category';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={ Home } />
-      </Switch>
+      <>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+        </Switch>
+        <Category />
+      </>
     );
   }
 }
